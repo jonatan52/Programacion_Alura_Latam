2dc4e59b68280469c127fd1029f2a8866fd8bb88
Jonatan <tarriba52@gmail.com>
2023-02-24 11:05:38 -0500

    trabjando con palntilla

---
+++ Curso JavaScript/Primeros pasos/06-PlantillaTexto.js
@@ -0,0 +1,6 @@
+const nombrePasajero = 'Leonardo'
+const apellidoPasajero = 'La cruz'
+
+let nombreCompleto = `El nombre complesto es: ${nombrePasajero} ${apellidoPasajero}`;
+
+console.log(nombreCompleto);
