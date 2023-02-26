21625b24f84cde1ab817fd394c790aa2b7176cca
Jonatan <tarriba52@gmail.com>
2023-02-26 18:00:24 -0500

    finalizando el curso

--- Curso JavaScript/Primeros pasos/07-listas.js
+++ Curso JavaScript/Primeros pasos/07-listas.js
@@ -1,2 +1,25 @@
-const listaDePaisese = new Array ("Bogota", "Cali", "Medellin")
-console.log(listaDePaisese)
+const listaDePaises = new Array ("Colombia", "Brasil", "Argentina");
+console.log(listaDePaises);
+
+// Forma abreviada de crear un Array
+const ciudadesDisponibles = ["Cartagena", "Bucaramnga", "Pereira"];
+console.log(ciudadesDisponibles );
+
+//push: Agrega un nuevo elemnto al final del arreglo
+listaDePaises.push("Nicaragua"); 
+console.log(listaDePaises);
+
+//Mostrar elementos por posicion
+console.log(listaDePaises[0])
+
+//length: muetra la cantidad de elementos que tiene la lista
+console.log(`La lista contiene ${listaDePaises.length} elementos`); 
+
+// Remueve el primer elemto de la lista
+listaDePaises.shift();
+
+// Remueve el ultimo elemto de la lista
+listaDePaises.pop();
+
+//
+listaDePaises.filter();
---
+++ Curso JavaScript/Primeros pasos/08-OperadoresLogicos.js
@@ -0,0 +1,11 @@
+
+const ciudadDestino = "Bogota";
+const listaDePaises = new Array ("Bogota", "Colombia", "Brasil", "Argentina");
+// console.log(listaDePaises);
+
+if(listaDePaises.indexOf(ciudadDestino) >=0){
+    console.log("Pasaje disponible")
+}
+else{
+    console.log("No esta disponible")
+}
